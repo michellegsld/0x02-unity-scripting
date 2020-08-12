23ab1dfd63ad44c88c0972553aef4ac058ad86a9
Michelle Giraldo <michellegsld@gmail.com>
2020-08-11 20:20:25 -0400

    [Update] Move camera to better position, add Rigidbody to Player, and create script for Player to move around WASD/Arrows

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2.5f * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKeyDown("w"))
+        {
+            Vector3 position = this.transform.position;
+            position.z += speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKeyDown("a"))
+        {
+            Vector3 position = this.transform.position;
+            position.x += speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKeyDown("s"))
+        {
+            Vector3 position = this.transform.position;
+            position.z -= speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKeyDown("d"))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= speed;
+            this.transform.position = position;
+        }
+    }
+}
